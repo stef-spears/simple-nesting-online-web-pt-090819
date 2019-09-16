8ec005f8deac09117c2edfc13cc004ea9a62ba7b
Stef Spears <spears.sj87@gmail.com>
2019-09-16 01:54:59 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -59,7 +59,7 @@ def dennis_ritchies_language
         }
      }
      
-     programmer_hash[:dennis_ritchie][:languages].to_s 
+     programmer_hash[:dennis_ritchie][:languages]
      
 end
 
@@ -108,8 +108,8 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -128,5 +128,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+programmer_hash[]
 end
